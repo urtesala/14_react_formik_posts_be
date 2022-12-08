97ef2559ad6be216d6076820f4e68c9d8b3be468
urtesala <urtesaladziute@gmail.com>
2022-12-08 09:41:42 +0300

    npm i router, two pages

---
+++ src/pages/AddPostPage.jsx
@@ -0,0 +1,9 @@
+function AddPostPage(props) {
+  return (
+    <div>
+      <h1>Post Page</h1>
+      <p>this should be a form</p>
+    </div>
+  );
+}
+export default AddPostPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage(props) {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>welcome</p>
+    </div>
+  );
+}
+export default HomePage;
